2604263d55cc42f7d4b018ad86d3c1efddf72309
Aayush Pudasaini <65882544+aayuspudasaini@users.noreply.github.com>
2021-11-02 22:31:16 +0545

    Add files via upload

---
+++ obs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sudo apt install libavutil56=7:4.1.8-0+deb10u1 -y
+
+sudo apt install ffmpeg -y
+
+ sudo apt-get update && sudo apt-get upgrade -y 
+
+sudo apt-get install obs-studio -y
+
+sudo apt-get autoremove
+
+echo " Installation Successful !! " 
+
+
+
